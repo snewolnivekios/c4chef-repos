7fec3107ae99db83bbbeb86cbeb0aac44947f918
snewolk <snewolk@pm.me>
2021-06-11 21:07:35 +0000

    Initial commit

---
+++ cookbooks/apache/recipes/default.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook:: apache
+# Recipe:: default
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.
+package 'apache2' do
+  action :install
+  case node[:platform]
+  when 'redhat','centos'
+    package_name 'httpd'
+    action    :install
+  when 'ubuntu','debian'
+    package_name 'apache2'
+    action :install
+  end
+end
+
+service "apache2" do
+  case node[:platform]
+  when 'redat','centos'
+    service_name 'httpd'
+  when 'ubuntu','debian'
+    service_name 'apache2'
+  end
+  action [ :enable, :start ]
+end
+
+template "/var/www/html/index.html" do
+  source 'index.html.erb'
+  mode '0644'
+end
---
+++ cookbooks/apt/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook:: apt
+# Recipe:: default
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.
+
+execute "apt-get update" do
+  command "apt-get update"
+end
+
